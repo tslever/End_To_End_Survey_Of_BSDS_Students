1fe337c6ea35ef9bfd4ef0e46627363996d922b8
Tom Lever <46533664+tslever@users.noreply.github.com>
2024-07-12 17:55:58 -0400

    Plotted smooth probability distribution for "The BSDS program was satisfying overall."

--- Plot_Probability_Distributions_Of_Scores_For_The_BSDS_Program_Is_Satisfying_Overall.py
+++ Plot_Probability_Distributions_Of_Scores_For_The_BSDS_Program_Is_Satisfying_Overall.py
@@ -1,17 +1,17 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import numpy as np
 
 data_frame_of_features = pd.read_csv("Feature_Matrix.csv", header = 0, index_col = 0)
 series_of_scores = pd.to_numeric(data_frame_of_features.iloc[0][2:])
-score_counts = series_of_scores.value_counts().sort_index()
-probabilities = score_counts / score_counts.sum()
-plt.bar(probabilities.index, probabilities.values, width = 1.0, edgecolor = "black", alpha = 0.6, label = "Probability Distribution")
-sns.kdeplot(series_of_scores, bw_adjust = 0.5, fill = True, label = "KDE", color = "blue")
-plt.title("Probability Distribution of Scores for \"The BSDS program was satisfying overall.\"")
+hist_data = sns.histplot(series_of_scores, bins = 10, kde = True)
+plt.title("Probability Distributions for \"The BSDS program was satisfying overall.\"")
 plt.xlabel("Scores")
-plt.ylabel("Probability")
-plt.xticks(probabilities.index)
+plt.ylabel("Probability Density")
+bin_edges = np.histogram_bin_edges(series_of_scores, bins = 10)
+bin_centers = 0.5 * (bin_edges[:-1] + bin_edges[1:])
+plt.xticks(bin_centers, labels = range(1, 11))
 plt.grid()
 plt.legend()
 plt.show()
