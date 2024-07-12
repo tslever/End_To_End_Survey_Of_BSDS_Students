acd9a754c9c6f7d6a7175642ae019bbfc8ac1d95
Tom Lever <46533664+tslever@users.noreply.github.com>
2024-07-12 19:09:33 -0400

    Cleaned up code to display table of numbers of BSDS students with genders

--- Display_Table_Of_Numbers_Of_BSDS_Students_With_Genders.py
+++ Display_Table_Of_Numbers_Of_BSDS_Students_With_Genders.py
@@ -1,8 +1,6 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 
 data_frame_of_features = pd.read_csv("Feature_Matrix.csv", header = 0, index_col = 0)
 series_of_genders = data_frame_of_features.iloc[521][2:]
-counts = series_of_genders.value_counts().sort_index()
-print(type(counts))
-print(counts)
+series_of_counts = series_of_genders.value_counts().sort_index()
+print(series_of_counts)
