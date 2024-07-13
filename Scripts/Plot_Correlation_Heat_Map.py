688f43ca0e8dc08082252dbb080c157c06007e28
Tom Lever <46533664+tslever@users.noreply.github.com>
2024-07-13 01:22:54 -0400

    Plotted correlation heat map of rows 0, 1, and 2

---
+++ Scripts/Plot_Correlation_Heat_Map.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data_frame_of_features = pd.read_csv("Feature_Matrix.csv", header = 0, index_col = 0)
+series_of_scores_in_row_0 = pd.to_numeric(data_frame_of_features.iloc[0][2:])
+series_of_scores_in_row_1 = pd.to_numeric(data_frame_of_features.iloc[1][2:])
+series_of_scores_in_row_2 = pd.to_numeric(data_frame_of_features.iloc[2][2:])
+scores_df = pd.DataFrame({
+    "Row 0": series_of_scores_in_row_0,
+    "Row 1": series_of_scores_in_row_1,
+    "Row 2": series_of_scores_in_row_2
+})
+correlation_matrix = scores_df.corr()
+sns.heatmap(correlation_matrix, annot = True, cmap = 'coolwarm', linewidths = 0.5)
+plt.title('Correlation Heat Map of Scores')
+plt.show()
